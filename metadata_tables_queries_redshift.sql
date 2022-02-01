8e6a8d28898ad4beee9e587013eb3abd209bd76c
Jordi Sanchez (Legion) <jordineil8@gmail.com>
2022-02-01 12:25:02 -0500

    se agregan dos nuevos comandos útiles

--- metadata_tables_queries_redshift.sql
+++ metadata_tables_queries_redshift.sql
@@ -1,5 +1,5 @@
 SELECT * FROM pg_table_def
-WHERE tablename = 'sales'; -- encode, distribucion, ordenamiento
+WHERE tablename = 'sales'; -- encode, distribución, ordenamiento
 
 SELECT * FROM pg_catalog.stv_blocklist sb ; -- permite ver los bloques de datos
 
@@ -7,14 +7,13 @@ SELECT * FROM pg_catalog.stl_load_errors ; -- logs de los errores
 
 SELECT * FROM pg_catalog.stl_load_commits ; -- logs de los commits quedaron bien
 
-SELECT * FROM stl_query; -- informacion de los queries
+SELECT * FROM stl_query; -- información de los queries
 
 SELECT * FROM stl_query
 WHERE query = 29;
 
 SELECT * FROM pg_catalog.svl_qlog  -- ver queries
 ORDER BY starttime desc;
-
 
 SELECT * FROM pg_catalog.svl_user_info ; -- informacion de los usuarios
 
@@ -24,3 +23,7 @@ SELECT * FROM svv_tables;
 
 SELECT * FROM svv_tables
 WHERE table_schema = 'public';
+
+analyze compression table --analizar compresion por si se puede mejorar
+
+--COMPUPDATE -- actualiza la compresión a la más óptima, útil al cargar los datos con copy
